c7fe76754490b8e8aae3d49e3ef31d257d44d75a
Logan Finley <finley4.logan@gmail.com>
2023-05-05 23:39:10 -0700

    Set up simple poker game

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,21 @@
 
 int main()
 {
-    std::cout << "Hello there" << std::endl;
+    Deck deck;
+
+    Player p1;
+    Player p2;
+
+    p1.add_card(deck.draw());
+    p2.add_card(deck.draw());
+    p1.add_card(deck.draw());
+    p2.add_card(deck.draw());
+
+    std::cout << "player 1 hand:\n";
+    p1.show_hand();
+
+    std::cout << '\n';
+
+    std::cout << "player 2 hand:\n";
+    p2.show_hand();
 }
