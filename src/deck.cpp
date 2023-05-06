82bee892a6190d6d29f4b12772b2b8191c7f2f5f
Logan Finley <finley4.logan@gmail.com>
2023-05-05 22:56:46 -0700

    Add deck class (minus the shuffle method)

---
+++ src/deck.cpp
@@ -0,0 +1,29 @@
+#include "deck.h"
+#include "card.h"
+
+#include <memory>
+#include <vector>
+
+Deck::Deck()
+{
+    for (int suit = CLUBS; suit <= SPADES; suit++) {
+        for (int rank = ACE; rank <= KING; rank++) {
+            cards.push_back(std::make_unique<Card>(static_cast<Rank>(rank), static_cast<Suit>(suit)));
+        }
+    }
+}
+
+void Deck::shuffle()
+{
+}
+
+std::unique_ptr<Card> Deck::draw()
+{
+    if (cards.empty()) {
+        return nullptr;
+    }
+
+    std::unique_ptr<Card> card = std::move(cards.back());
+    cards.pop_back();
+    return card;
+}
--- src/deck.h
+++ src/deck.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "card.h"
+
+#include <memory>
+#include <vector>
+
+class Deck {
+    public:
+        Deck();
+
+        void shuffle();
+        std::unique_ptr<Card> draw();
+
+    private:
+        std::vector<std::unique_ptr<Card> > cards;
+};
