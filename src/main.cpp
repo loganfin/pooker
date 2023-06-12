2f856fcaf98cf3cba0ed6ebb91c4db41d5125db7
loganfin <finely4.logan@gmail.com>
2023-06-12 12:41:06 -0700

    Make example test loop more similar to texas holdem

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 int main()
 {
     Deck deck;
+    deck.shuffle();
     std::vector<Hand> hands{2};
 
     std::vector<Player> players;
@@ -16,13 +17,17 @@ int main()
     players.push_back(Player("jeff"));
 
     for (int i{}; i < 5; i++) {
+        std::cout << "hand: " << i << '\n';
         deck.shuffle();
         for (auto& player : players) {
+            std::cout << player.get_name() << '\n';
             player.fill_hand(deck.make_hand(2));
             for (const auto& card : player.show_hand()) {
                 std::cout << *card << '\n';
             }
+        }
 
+        for (auto& player : players) {
             deck.push_hand(player.return_hand());
         }
     }
